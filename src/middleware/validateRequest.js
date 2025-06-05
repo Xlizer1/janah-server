318634cb85db86bff2794841da8937251d80040e
xlizer1 <mustafa_142@mail.ru>
2025-06-06 01:53:41 +0300

    save

--- src/middleware/validateRequest.js
+++ src/middleware/validateRequest.js
@@ -1,20 +1,20 @@
 const validateRequest = (schema) => {
-    return (req, res, next) => {
-        const payload = {
-            ...req.body,
-            ...req.params,
-            ...req.query,
-            // image: req.file || req.files?.image
-        };
-        const { error } = schema.validate(payload);
-        if (error) {
-            return res.status(400).json({
-                status: "error",
-                message: error.details[0].message,
-            });
-        }
-        next();
+  return (req, res, next) => {
+    const payload = {
+      ...req.body,
+      ...req.params,
+      ...req.query,
+      // image: req.file || req.files?.image
     };
+    const { error } = schema.validate(payload);
+    if (error) {
+      return res.status(400).json({
+        status: "error",
+        message: error.details[0].message,
+      });
+    }
+    next();
+  };
 };
 
 module.exports = validateRequest;
